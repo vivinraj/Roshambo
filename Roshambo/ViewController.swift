5f612bc86633fca5ebd742acb55d8f7b3c44ba44
Vivin Raj <vivinraj@vivins-macbook-air.local>
2016-05-07 18:10:04 +0530

    Initial Commit

---
+++ Roshambo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Vivin Raj on 07/05/16.
+//  Copyright © 2016 Vivin Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
